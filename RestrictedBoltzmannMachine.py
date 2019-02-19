7a0055294ca28e1c070a195bca964ba7021d575d
christoph <christoph.kirchner@student.uibk.ac.at>
2019-02-19 19:00:52 +0100

    refactor

--- RestrictedBoltzmannMachine.py
+++ RestrictedBoltzmannMachine.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 class RBM:
 
     def __init__(self, num_visible, num_hidden):
-        self.num_hidden = num_hidden
-        self.num_visible = num_visible
+        self.num_hidden_node = num_hidden
+        self.num_visible_nodes = num_visible
 
         np_rng = np.random.RandomState(1234)
 
@@ -19,19 +19,19 @@ class RBM:
         self.weights = np.insert(self.weights, 0, 0, axis = 0)
         self.weights = np.insert(self.weights, 0, 0, axis = 1)
 
-    def getRandomSamples(self, data, k=1000):
-        samples = np.zeros((k, self.num_visible+1), dtype=np.dtype('b'))
+    def getRandomSamples(self, data, k=2000):
+        samples = np.zeros((k, self.num_visible_nodes+1), dtype=np.dtype('b'))
         for i in range(k):
             samples[i] = data[randint(0, data.shape[0]-1)]
         return samples
 
-    def initRandomSamples(self, k=1000):
-        samples = np.ones((k, self.num_visible + 1))
+    def initRandomSamples(self, k=2000):
+        samples = np.ones((k, self.num_visible_nodes + 1))
         for i in range(k):
-            samples[i,1:] = np.random.choice([0,1],self.num_visible)
+            samples[i,1:] = np.random.choice([0,1],self.num_visible_nodes)
         return samples
 
-    def train(self, data, max_epochs=1000, learning_rate=0.1, m=1000, k=5):
+    def train(self, data, max_epochs=1000, learning_rate=0.1, m=2000, k=5):
         initSamples = self.initRandomSamples()
         num_examples = data.shape[0]
 
@@ -42,10 +42,10 @@ class RBM:
             samples = self.getRandomSamples(data)
 
             # positive phase
-            pos_hidden_activations = np.dot(samples, self.weights)
-            pos_hidden_probs = self._logistic(pos_hidden_activations)
-            pos_hidden_probs[:,0] = 1 # bias unit
-            pos_hidden_states = pos_hidden_probs > np.random.rand(m, self.num_hidden + 1)
+            pos_activations = np.dot(samples, self.weights)
+            pos_probs = self._logistic(pos_activations)
+            pos_probs[:,0] = 1 # bias unit
+            pos_hidden_states = pos_probs > np.random.rand(m, self.num_hidden_node + 1)
             pos_associations = np.dot(samples.T, pos_hidden_states)
 
             # negative phase
@@ -54,46 +54,46 @@ class RBM:
                 hidden_activations = np.dot(initSamples, self.weights)
                 hidden_probs = self._logistic(hidden_activations)
                 hidden_probs[:,0] = 1 # bias unit.
-                hidden_states = hidden_probs > np.random.rand(self.num_hidden + 1)
+                hidden_states = hidden_probs > np.random.rand(self.num_hidden_node + 1)
                 hidden_states[:0] = 1
 
                 # Recalculate the probabilities that the visible units are on.
                 visible_activations = np.dot(hidden_states, self.weights.T)
                 visible_probs = self._logistic(visible_activations)
                 visible_probs[:,0] = 1 # bias unit.
-                initSamples = visible_probs > np.random.rand(self.num_visible + 1)
+                initSamples = visible_probs > np.random.rand(self.num_visible_nodes + 1)
 
-            neg_visible_activations = np.dot(hidden_states, self.weights.T)
-            neg_visible_probs = self._logistic(neg_visible_activations)
-            neg_visible_probs[:,0] = 1 # bias unit.
-            neg_hidden_activations = np.dot(neg_visible_probs, self.weights)
+            neg_activations = np.dot(hidden_states, self.weights.T)
+            neg_probs = self._logistic(neg_activations)
+            neg_probs[:,0] = 1 # bias unit.
+            neg_hidden_activations = np.dot(neg_probs, self.weights)
             neg_hidden_probs = self._logistic(neg_hidden_activations)
-            neg_associations = np.dot(neg_visible_probs.T, neg_hidden_probs)
+            neg_associations = np.dot(neg_probs.T, neg_hidden_probs)
 
             # Update weights
             self.weights += learning_rate * ((pos_associations - neg_associations) / num_examples)
 
-            error = np.sum((samples - neg_visible_probs) ** 2)
+            error = np.sum((samples - neg_probs) ** 2)
             print("Epoch %s: error is %s" % (epoch, error))
 
 
 
-    def daydream(self, num_samples):
-        samples = np.ones((num_samples, self.num_visible + 1))
-        samples[0,1:] = np.random.choice([0,1],self.num_visible)
+    def markovChain(self, num_samples):
+        samples = np.ones((num_samples, self.num_visible_nodes + 1))
+        samples[0,1:] = np.random.choice([0,1],self.num_visible_nodes)
 
         for i in range(1, num_samples):
             visible = samples[i-1,:]
 
             hidden_activations = np.dot(visible, self.weights)
             hidden_probs = self._logistic(hidden_activations)
-            hidden_states = hidden_probs > np.random.rand(self.num_hidden + 1)
+            hidden_states = hidden_probs > np.random.rand(self.num_hidden_node + 1)
             hidden_states[0] = 1
 
             # Recalculate the probabilities that the visible units are on.
             visible_activations = np.dot(hidden_states, self.weights.T)
             visible_probs = self._logistic(visible_activations)
-            visible_states = visible_probs > np.random.rand(self.num_visible + 1)
+            visible_states = visible_probs > np.random.rand(self.num_visible_nodes + 1)
             samples[i,:] = visible_states
 
         # Ignore the bias units (the first column), since they're always set to 1.
@@ -103,13 +103,13 @@ class RBM:
         return 1.0 / (1 + np.exp(-x))
 
 if __name__ == '__main__':
-    r = RBM(num_visible = 784, num_hidden = 784)
+    r = RBM(num_visible=784, num_hidden=784)
     dt = np.dtype('>u4, >u4, >u4, >u4, (10000,784)u1')
     mnist = np.fromfile('t10k-images-idx3-ubyte', dtype=dt)['f4'][0]
     imgs = np.zeros((10000, 784), dtype=np.dtype('b'))
     imgs[mnist > 127] = 1
-    r.train(imgs, max_epochs = 500)
-    generated = r.daydream(10)
+    r.train(imgs, max_epochs = 5000)
+    generated = r.markovChain(10)
 
     for img in generated:
         first_image = img
